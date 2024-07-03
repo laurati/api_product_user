fca5de9030084bb6f77b3495b8bac9fdf30e8881
LAURA MARQUEZ <LMARQUEZ@ext.worten.pt>
2024-07-03 17:28:56 +0100

    jwt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/laurati/api_product_user/configs"
 	"github.com/laurati/api_product_user/internal/entity"
 	"github.com/laurati/api_product_user/internal/infra/database"
@@ -14,7 +16,7 @@ import (
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
+	configs, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
@@ -27,18 +29,23 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
 
 	// logs the start and end of each request
 	r.Use(middleware.Logger)
+	// r.Use(LogRequest)
 	// absorve panics and prints the stack trace
 	r.Use(middleware.Recoverer)
-	// r.Use(middleware.WithValue("jwt", configs.TokenAuth))
-	// r.Use(middleware.WithValue("JwtExperesIn", configs.JwtExperesIn))
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExperesIn", configs.JwtExpIresIn))
 
 	r.Route("/products", func(r chi.Router) {
-		// r.Use(jwtauth.Verifier(configs.TokenAuth))
-		// r.Use(jwtauth.Authenticator)
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
 		r.Post("/", productHandler.CreateProduct)
 		r.Get("/", productHandler.GetProducts)
 		r.Get("/{id}", productHandler.GetProduct)
@@ -46,5 +53,17 @@ func main() {
 		r.Delete("/{id}", productHandler.DeleteProduct)
 	})
 
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.Create)
+		r.Get("/generate_token", userHandler.GetJWT)
+	})
+
 	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/infra/webserver/handlers/user_handler.go
+++ internal/infra/webserver/handlers/user_handler.go
@@ -1,6 +1,20 @@
 package handlers
 
-import "github.com/laurati/api_product_user/internal/infra/database"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/laurati/api_product_user/internal/dto"
+
+	"github.com/go-chi/jwtauth"
+	"github.com/laurati/api_product_user/internal/entity"
+	"github.com/laurati/api_product_user/internal/infra/database"
+)
+
+type Error struct {
+	Message string `json:"message"`
+}
 
 type UserHandler struct {
 	UserDB database.UserInterface
@@ -10,4 +24,59 @@ func NewUserHandler(db database.UserInterface) *UserHandler {
 	return &UserHandler{
 		UserDB: db,
 	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
+	jwtExpiresIn := r.Context().Value("JwtExperesIn").(int)
+
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		err := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, _ := jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
+	})
+	accessToken := dto.GetJWTOutput{AccessToken: tokenString}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
+}
+
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
